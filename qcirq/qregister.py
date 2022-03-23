b3b1cc7608e5484eefb2c2f283abeec8e1b1e101
j-henkel <jan.henkel.1@web.de>
2022-03-23 18:24:47 +0100

    implemented inspect_entaglement

--- qcirq/qregister.py
+++ qcirq/qregister.py
@@ -14,7 +14,7 @@ class QRegister:
         """
 
         max_qbits = 10
-        qbits = np.log2(len(state))
+        qbits = int(np.log2(len(state)))
         norm = np.linalg.norm(state, 2)
         max_norm_deviation = 1e-9
         assert 1.0 - max_norm_deviation <= norm <= 1.0 + max_norm_deviation, \
@@ -29,8 +29,17 @@ class QRegister:
 
     def inspect_entanglement(self):
         """check wether the register is entangled"""
-        #TODO implement test_for_entanglement()
-        return
+        separable = np.zeros(self.qbits-1)
+        register = self.state.copy()
+
+        for i in range(1, self.qbits):
+            m = register.reshape(2**i, int(len(register)/(2**i)))
+            rank = np.linalg.matrix_rank(m)
+            separable[i-1] = not (rank > 1)
+
+        # might struggle with rounding of last digit of float numbers 
+        # for example 0.99999999999999 instead of 1.0
+        return separable
 
     def separate(self):
         """if the register is not entangled return the states of the single
--- qcirq/test/test_qregister.py
+++ qcirq/test/test_qregister.py
@@ -27,3 +27,25 @@ class TestQRegister:
         except AssertionError:
             assert False
 
+    def test_entanglement(self):
+        #TODO edge cases like one single qbit
+        isqr2 = 1/np.sqrt(2)
+        entangled2 = np.array([isqr2, 0, 0, isqr2])
+        unentangled = np.array([isqr2, isqr2])
+        qreg = QRegister(entangled2)
+        result = qreg.inspect_entanglement()
+        assert (result == np.array([False])).all()
+        qreg = QRegister(np.kron(entangled2, unentangled))
+        result = qreg.inspect_entanglement()
+        assert (result == np.array([False, True])).all()
+        qreg = QRegister(np.kron(entangled2, entangled2))
+        result = qreg.inspect_entanglement()
+        assert (result == np.array([False, True, False])).all()
+        qreg = QRegister(np.kron(unentangled, entangled2))
+        result = qreg.inspect_entanglement()
+        assert (result == np.array([True, False])).all()
+        x = np.kron(entangled2, unentangled)
+        x = np.kron(x, entangled2)
+        qreg = QRegister(x)
+        result = qreg.inspect_entanglement()
+        assert (result == np.array([False, True, True, False])).all()
